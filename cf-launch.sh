00fd0efa28338940d2ff2ad669d5cce4d527108d
Software factory <factory@coronadoland.com>
2024-12-04 19:08:21 +0100

    simplifing

--- cf-launch.sh
+++ cf-launch.sh
@@ -10,7 +10,7 @@
 # Date: 2024-10-03
 #
 
-
+git="https://raw.githubusercontent.com/coronadofactory"
 
 re="(install)"
 if [[ $1 =~ $re ]]; then
@@ -29,18 +29,15 @@ fi
 re="(install)"
 if [[ $cmd =~ $re ]]; then
 
-   re="(react|webpack|appia|backend|form|proxy)"
-   if [[ $1 =~ $re ]]; then
-      curl -s https://raw.githubusercontent.com/coronadofactory/hexagonal/refs/heads/main/front/cf.sh | sh -s $1
+   front="(react|webpack|appia|backend|form|proxy)"
+   devops="(cookies|analytics|youtube)"
+   if [[ $1 =~ $front ]]; then
+      curl -s $git/hexagonal/refs/heads/main/front/cf.sh | sh -s $1
+   elif [[ $1 =~ $devops ]]; then
+      curl -s $git/site-builder/refs/heads/master/cf.sh | sh -s $1
    else
-      re="(cookies|analytics|youtube)"
-      if [[ $1 =~ $re ]]; then
-         curl -s https://raw.githubusercontent.com/coronadofactory/site-builder/refs/heads/master/cf.sh | sh -s $1
-      else
-         echo "Unknown $1"
-      fi
+      echo "Unknown $1"
+      exit
    fi
-
-   exit
 
 fi
